f1203919aa566f3f8fa3b00f89f38b3bf6392c56
han18 <han.fadah@gmail.com>
2023-11-21 10:51:27 -0500

    Compeletd E. Driver's Ed

--- Section1/src/index.js
+++ Section1/src/index.js
@@ -78,3 +78,15 @@ if (animal === "cow") {
 }
 
 // Change your code so that if the variable animal is anything other than a cow, it will print "Hey! You're not a cow." Commit
+
+// ==================== E. Driver's Ed ===================
+// Make a variable that holds a person's age; be semantic
+// Write code that will print out "Here are the keys!", if the age is 16 years or older, or, if the age is younger than 16, a message should print "Sorry, you're too young."
+
+const age = 12;
+
+if (age >= 16) {
+  console.log("Here are the keys!");
+} else {
+  console.log("Sorry, you're too young.");
+}
