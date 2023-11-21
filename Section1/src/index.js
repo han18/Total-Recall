6ac49f434f920561685c566b06e07525f964336b
han18 <han.fadah@gmail.com>
2023-11-21 10:18:45 -0500

    Did practice A & B

---
+++ Section1/src/index.js
@@ -0,0 +1,49 @@
+// 1. Variables & Data types
+
+// A. Variables
+
+// Declare a new constant variable called myName and assign it your name?
+
+const myName = "Hanan";
+
+//Declare a variable called favoriteLanguage and assign it the value of Python?
+const favoriteLanguage = "Python";
+
+//Declare a new variable called newFavoriteLanguage and assign it JavaScript;
+let newFavoriteLanguage = "JavaScript";
+
+//Assign the value of newFavoriteLanguage to favoriteLanguage referencing each other.
+newFavoriteLanguage = favoriteLanguage;
+console.log(newFavoriteLanguage);
+
+//Finally log or print a message using a Literal String, make use of the variables myName and favoriteLanguage.
+console.log(
+  `my name is ${myName} and my favorite programming language is ${favoriteLanguage}`
+);
+
+// B. Strings
+//Create a variable called firstVariable
+//Assign it the value of the string "Hello World"
+
+let firstVariable = "Hello World";
+
+// Change the value of this variable to some number
+firstVariable = 100;
+
+// Store the value of firstVariable in a new variable called secondVariable
+firstVariable = secondVariable = firstVariable;
+console.log(secondVariable);
+
+//Change the value of secondVariable to any string.
+
+secondVariable = "classwork";
+
+//What is the value of firstVariable? Log it!
+console.log(firstVariable);
+
+/* Create a variable called yourName and set it equal to your name as a string. Then, write an expression that takes the string "Hello, my name is " and the variable yourName so that it returns a new string with them concatenated. ex: Hello, my name is Jean Valjean */
+
+const yourName = "Hanan";
+console.log("Hello, my name is " + yourName);
+
+// =========== C. Booleans ==============
